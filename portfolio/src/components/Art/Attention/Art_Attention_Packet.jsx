a11d1e39a68aad9795dce1c09d66a8688781fcd7
Nicholas Mirigliani <nicholasmirigliani@gmail.com>
2025-10-28 19:55:27 -0400

    tailwind attention

--- portfolio/src/components/Art/Attention/Art_Attention_Packet.jsx
+++ portfolio/src/components/Art/Attention/Art_Attention_Packet.jsx
@@ -1,35 +1,30 @@
 // import { SnavLink } from "react-router-dom";
 import SnavLink from "../../SnavLink";
+import { NavLink } from "react-router-dom";
 import Navigation from "../../Navigation";
 
 function Art_Attention() {
-  const body = document.body;
-  body.className = "default-bg";
   const pageList = Array.from({ length: 18 }, (_, i) => (i + 1).toString());
   return (
-    <div className="attentionOuterContainer">
-      <div className="NavigationDiv">
-        <Navigation scrollText="ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION!" />
+    <div className="flex flex-col items-center justify-center min-h-screen font-roboto gap-4">
+    <NavLink to={"/"}>Nicholas Mirigliani</NavLink>
+    <Navigation />
+      <img
+        className="max-w-3xs"
+        src={`/attention/title.png`}
+      />
+      <SnavLink to="/art/attention/">BACK TO MAIN PAGE</SnavLink>
+      <h2>PART TWO: THE BOOK</h2>
+      <p>PLEASE FIND THE PAGES OF THE INSTRUCTION BOOK BELOW.</p>
+      <div className="flex flex-col items-center gap-4">
+        {pageList.map((page) => (
+          <img
+            className="max-w-1/2 border-2"
+            src={`/attention/packet_pages/${page}.png`}
+          />
+        ))}
       </div>
-      <div className="attentionBody">
-        <img
-          className="attentionTitle"
-          src={`/attention/title.png`}
-        />
-        <br></br>
-        <SnavLink to="/art/attention/">BACK TO MAIN PAGE</SnavLink>
-        <h2>PART TWO: THE BOOK</h2>
-        <p>PLEASE FIND THE PAGES OF THE INSTRUCTION BOOK BELOW.</p>
-        <div className="attentionPageList">
-          {pageList.map((page) => (
-            <img
-              className="attentionPage"
-              src={`/attention/packet_pages/${page}.png`}
-            />
-          ))}
-        </div>
-        <SnavLink to="/art/attention/">BACK TO MAIN PAGE</SnavLink>
-      </div>
+      <SnavLink to="/art/attention/">BACK TO MAIN PAGE</SnavLink>
     </div>
   );
 }
--- portfolio/src/components/Art/Attention/Art_Attention_Presentation.jsx
+++ portfolio/src/components/Art/Attention/Art_Attention_Presentation.jsx
@@ -1,35 +1,32 @@
 // import { SnavLink } from "react-router-dom";
 import SnavLink from "../../SnavLink";
 import Navigation from "../../Navigation";
+import { NavLink } from "react-router-dom";
 function Art_Attention() {
   const body = document.body;
   body.className = "default-bg";
   const pageList = Array.from({ length: 23 }, (_, i) => (i + 1).toString());
   return (
-    <div className="attentionOuterContainer">
-      <div className="NavigationDiv">
-        <Navigation scrollText="ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION! ATTENTION!" />
-      </div>
-      <div className="attentionBody">
-        <img
-          className="attentionTitle"
-          src={`/attention/title.png`}
-        />
-        <br></br>
+    <div className="flex flex-col items-center justify-center min-h-screen font-roboto gap-4">
+    <NavLink to={"/"}>Nicholas Mirigliani</NavLink>
+    <Navigation />
+      <img
+        className="max-w-3xs"
+        src={`/attention/title.png`}
+      />
         <SnavLink to="/art/attention/">BACK TO MAIN PAGE</SnavLink>
         <h2>PART THREE: THE USB</h2>
         <p>PLEASE FIND THE SLIDES OF THE PRESENTATION BELOW.</p>
-        <div className="attentionPageList">
+        <div className="flex flex-col items-center gap-4">
           {pageList.map((page) => (
             <img
-              className="attentionPage"
+              className="max-w-1/2 border-2"
               src={`/attention/presentation_slides/${page}.png`}
             />
           ))}
         </div>
         <SnavLink to="/art/attention/">BACK TO MAIN PAGE</SnavLink>
       </div>
-    </div>
   );
 }
 export default Art_Attention;
